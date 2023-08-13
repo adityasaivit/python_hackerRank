2bb43e652cfd31ceae64f5f3ef421d54fad68930
adityasaivit <113006142+adityasaivit@users.noreply.github.com>
2023-08-13 12:07:22 +0530

    Create power_mod.py

---
+++ power_mod.py
@@ -0,0 +1,8 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUbas=
+import math
+base=int(input())
+power=int(input())
+modulus=int(input())
+print(base**power)
+print((base**power)%modulus)
+
